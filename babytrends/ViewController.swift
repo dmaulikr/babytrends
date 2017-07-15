4dacc52411aab0101cd43ef9f9db60dac3e63d37
Alan Morris <dev@alanmorris.me>
2017-07-15 16:03:54 +0100

    Add swift project files

---
+++ babytrends/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  babytrends
+//
+//  Created by Alan Morris on 15/07/2017.
+//  Copyright © 2017 Alan Morris. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
